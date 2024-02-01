8cbbff80d276509668ccf6a7b8226bfa388a34ad
bajeanno <basiljeannot@gmail.com>
2024-02-01 15:59:29 +0100

    changed getType return type to const reference as the subject asks for it

--- cpp01/ex03/Weapon.cpp
+++ cpp01/ex03/Weapon.cpp
@@ -10,7 +10,7 @@ Weapon::Weapon(std::string type)
 	_type = type;
 }
 
-std::string	Weapon::getType()
+const std::string&	Weapon::getType()
 {
 	return _type;
 }
--- cpp01/ex03/Weapon.hpp
+++ cpp01/ex03/Weapon.hpp
@@ -7,7 +7,7 @@ class Weapon
 public:
 	Weapon();
 	Weapon(std::string type);
-	std::string	getType();
+	const std::string&	getType();
 	void		setType(std::string type);
 
 private:
