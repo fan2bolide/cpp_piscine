e0be0a0c8608073aa5ca1336202e980fec7d36e5
Basile Jeannot <bajeanno@student.42.fr>
2024-04-03 15:37:16 +0200

    finished ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,14 +1,83 @@
-//
-// Created by bajeanno on 4/3/24.
-//
-
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
 
+template <typename T>
 class Array {
-
+private:
+	T *_array;
+	const size_t _size;
+public:
+	Array();
+	Array(const unsigned int &n);
+	~Array();
+	size_t size();
+	void printArray();
+	Array<T> &operator=(const Array<T> &other);
+	T &operator[](const size_t &n);
+	T &operator[](const size_t &n) const;
+	class InvalidIndex : public std::exception {
+	public:
+		const char *what() const throw() {
+			return "exception: index is out of bound";
+		}
+	};
 };
 
+template<typename T>
+T &Array<T>::operator[](const size_t &n) {
+	if (n < 0 || n >= _size)
+		throw InvalidIndex();
+	return _array[n];
+}
 
-#endif //ARRAY_HPP
+template<typename T>
+T &Array<T>::operator[](const size_t &n) const {
+	if (n < 0 || n >= _size)
+		throw InvalidIndex();
+	return _array[n];
+}
+
+template<typename T>
+Array<T> &Array<T>::operator=(const Array<T> &other) {
+	if (this != &other) {
+		for (size_t i = 0;i < other._size;i++)
+			(*this)[i] = other[i];
+	}
+	return *this;
+}
+
+template<typename T>
+size_t Array<T>::size() {
+	return _size;
+}
+
+template<typename T>
+Array<T>::Array() : _size(0) {
+	_array = new T[0];
+	std::cout << "Array default constructor called" << std::endl;
+}
+
+template<typename T>
+Array<T>::Array(const unsigned int &n) : _size(n) {
+	_array = new T[n];
+	std::cout << "Array number constructor called" << std::endl;
+}
+
+template<typename T>
+Array<T>::~Array() {
+	delete _array;
+	std::cout << "Array destructor called" << std::endl;
+}
+
+template<typename T>
+void Array<T>::printArray() {
+	for (size_t i = 0 ; i < this->_size - 1 ; i++)
+		std::cout << _array[i] << ", ";
+	if (size() != 0)
+		std::cout << _array[_size - 1];
+	std::cout << std::endl;
+}
+
+#endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,3 +1,21 @@
-//
-// Created by bajeanno on 4/3/24.
-//
+#include <iostream>
+#include "Array.hpp"
+
+int main() {
+	Array<int> array(5);
+	Array<int> array2(5);
+	for (int i = 0;i < 5;i++) {
+		array[i] = i + 1;
+	}
+	std::cout << array[3] << std::endl;
+	try {
+		std::cout << array[1] << std::endl;
+		array2 = array;
+		array.printArray();
+	}
+	catch (Array<int>::InvalidIndex &e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << array.size() << std::endl;
+	return (0);
+}
