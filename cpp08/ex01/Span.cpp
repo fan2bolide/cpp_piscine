df3178d43293fb1746fc7a56e829eb3cf319a843
basil <basiljeannot@icloud.com>
2024-05-16 17:38:00 +0200

    fdj

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -1,1 +1,74 @@
 #include "Span.hpp"
+
+Span::Span() : array(new int[0]), _capacity(0), _size(0) {
+}
+
+Span::Span(size_t size) : _capacity(size), _size(0) {
+	array = new int[_capacity];
+	for (size_t i = 0; i < _capacity; i++) {
+		this->array[i] = 0;
+	}
+}
+
+Span::Span(const Span &other) : _capacity(other._capacity), _size(0) {
+	array = new int[_capacity];
+	for (size_t i = 0; i < _capacity; i++) {
+		this->array[i] = other.array[i];
+	}
+}
+
+Span::~Span() {
+	delete [] array;
+}
+
+Span &Span::operator=(const Span &other) {
+	if (this != &other) {
+		_size = 0;
+		delete [] array;
+		array = new int[_capacity];
+		for (size_t i = 0; i < _capacity; i++) {
+			this->array[i] = other.array[i];
+		}
+	}
+	return *this;
+}
+
+void Span::addNumber(int number) {
+	if (_size >= _capacity)
+		throw spanOverFlow();
+	array[_size] = number;
+	_size++;
+}
+
+int Span::min() {
+	int min = INT32_MIN;
+	for (size_t i = 0; i < _size; i++) {
+		if (min > array[i])
+			min = array[i];
+	}
+	return (min);
+}
+
+int Span::max() {
+	int max = INT32_MAX;
+	for (size_t i = 0; i < _size; i++) {
+		if (max < array[i])
+			max = array[i];
+	}
+	return (max);
+}
+
+int Span::longestSpan() {
+	return this->min() - this->max();
+}
+
+int Span::shortestSpan() {
+	int shortest = this->max();
+	if (_size < 2)
+		throw notEnoughNumbers();
+	for (size_t i = 0;i + 1 < _size;i++) {
+		
+	}
+}
+
+
---
+++ cpp08/ex01/Span.hpp
@@ -0,0 +1,35 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <cstddef>
+#include <exception>
+
+#define RED "\e[0;31m"
+#define COLOR_RESET "\e[0m"
+
+class Span {
+private:
+	Span();
+	int min();
+	int max();
+public:
+	Span(size_t size);
+	Span(const Span &other);
+	~Span();
+	Span &operator=(const Span &other);
+	void addNumber(int number);
+	class spanOverFlow : std::exception {
+		const char *what() const throw() {
+			return RED"Span: exception: span is already full"COLOR_RESET;
+		}
+	};
+	class notEnoughNumbers : std::exception {
+		const char *what() const throw() {
+			return RED"Span: exception: not enough numbers to compute span"COLOR_RESET;
+		}
+	};
+	int longestSpan();
+	int shortestSpan();
+};
+
+#endif
